5528293b78fcce9935521e1d8398c2a21abe2121
Porter <slugsaver@gmail.com>
2018-05-03 18:06:19 -0700

    Trying to use Jquery

--- web/week02.js
+++ web/week02.js
@@ -2,10 +2,17 @@ function clicked() {
 	alert("Clicked!");
 }
 
+
 function changecolor() {
+
+
+
+
 	var inputfield = document.getElementById("input").value;
 	alert(inputfield);
-	document.getElementById("unique").style.backgroundColor = inputfield;
-	alert("color has been changed!");
+	$("unique").css('background-color', inputfield);
+
+	// document.getElementById("unique").style.backgroundColor = inputfield;
+	// alert("color has been changed!");
 }
 
