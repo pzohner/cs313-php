4274b5c5f5db3a4a728b276f52cb4fa12f2c0460
Porter <slugsaver@gmail.com>
2018-05-03 17:53:04 -0700

    Changecolor function should be working

--- web/week02.js
+++ web/week02.js
@@ -3,5 +3,9 @@ function clicked() {
 }
 
 function changecolor() {
+	var inputfield = document.getElementsByName("fname").value;
+	alert(inputfield);
+	document.getElementById("unique").backgroundColor = inputfield;
 	alert("color has been changed!");
 }
+
