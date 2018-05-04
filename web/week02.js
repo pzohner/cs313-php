a7caa2851a915683174993719f7eb9490084b659
Porter <slugsaver@gmail.com>
2018-05-03 18:08:54 -0700

    Trying to use Jquery

--- web/week02.js
+++ web/week02.js
@@ -10,7 +10,7 @@ function changecolor() {
 
 	var inputfield = document.getElementById("input").value;
 	alert(inputfield);
-	$("unique").css('background-color', inputfield);
+	$("#unique").css('background-color', inputfield);
 
 	// document.getElementById("unique").style.backgroundColor = inputfield;
 	// alert("color has been changed!");
