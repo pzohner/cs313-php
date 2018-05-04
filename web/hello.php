1cc9b6f126ad0dbd374e227968b175a4b9083c79
Porter <slugsaver@gmail.com>
2018-05-04 09:58:58 -0700

    added full version of jquery

--- web/hello.php
+++ web/hello.php
@@ -5,7 +5,10 @@
 	 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>Week02 Team Assignment</title>
 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="<script
+			  src="https://code.jquery.com/jquery-3.3.1.js"
+			  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+			  crossorigin="anonymous"></script>"></script>
 	<script src="intro/scripts.js">
 	</script>
 
