548b615119b6012c2acabc1bb6258412ff8585ed
Porter <slugsaver@gmail.com>
2018-05-04 09:53:35 -0700

    fixed jquery

--- web/hello.php
+++ web/hello.php
@@ -6,23 +6,15 @@
 	<title>Week02 Team Assignment</title>
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="intro/scripts.js"></script>
+	<script src="intro/scripts.js">
+	</script>
 
 	<!-- Including bootstrap -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
 	<link href="intro/styles.css" rel="stylesheet" type="text/css">
 
-	<style>
-		.ifOnlyIKnew {
-			background-color: blue;
-			font-size: 24px;
-		}
 
-		.ifOnlyIKnew:hover {
-			font-weight: bold;
-		}
-	</style>
 	</head>
 <body>
 	<div id="unique" class="ifOnlyIKnew">
@@ -51,31 +43,12 @@
 
 
 
-
-
-
-
-
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 
 </body>
 <script>
-$("#changevisibility").click(function(){
 
-	if ($("#changevis").css("display") == "none") {
-		// alert("element is hidden: Showing now...")
-		$("#changevis").fadeIn();
-		// $("#changevis").css("visibility", "visible").hide().fadeIn("slow");;
-	} else {
-		// alert("element is visible: Hiding now...")
-		// $("#changevis").css("visibility", "hidden").hide().fadeIn();
-
-		// fadeOut sets display to "none" which is why I check for it above.
-		$("#changevis").fadeOut();
-	}
-
-});
 </script>
 </html>
--- web/intro/scripts.js
+++ web/intro/scripts.js
@@ -12,3 +12,23 @@ function changecolor() {
 	// document.getElementById("unique").style.backgroundColor = inputfield;
 	// alert("color has been changed!");
 }
+
+$(document).ready(function() {
+  $("#changevisibility").click(function(){
+
+  	if ($("#changevis").css("display") == "none") {
+  		// alert("element is hidden: Showing now...")
+  		$("#changevis").fadeIn();
+  		// $("#changevis").css("visibility", "visible").hide().fadeIn("slow");;
+  	} else {
+  		// alert("element is visible: Hiding now...")
+  		// $("#changevis").css("visibility", "hidden").hide().fadeIn();
+
+  		// fadeOut sets display to "none" which is why I check for it above.
+  		$("#changevis").fadeOut();
+  	}
+
+  });
+
+
+});
