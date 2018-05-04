a0da848e23ed791e632d53c266913cfd89dca9b5
Porter <slugsaver@gmail.com>
2018-05-04 09:47:07 -0700

    added files

---
+++ web/intro/scripts.js
@@ -0,0 +1,14 @@
+function clicked() {
+	alert("Clicked!");
+}
+
+
+function changecolor() {
+	var inputfield = document.getElementById("input").value;
+	alert(inputfield);
+	$("#unique").css('background-color', inputfield);
+
+	// This is if you wanted to do the above in javascript
+	// document.getElementById("unique").style.backgroundColor = inputfield;
+	// alert("color has been changed!");
+}
