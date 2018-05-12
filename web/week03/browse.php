a32eefaedc3531fbbdea0ae2167d5ad1a341e0b5
Porter <slugsaver@gmail.com>
2018-05-12 10:56:45 -0700

    working on week03

--- web/week03/browse.php
+++ web/week03/browse.php
@@ -39,7 +39,7 @@
       session_start();
       $itemName = $_POST["itemName"];
       $_SESSION["name"] = $_POST["data"];
-      
+      echo $_SESSION["name"];
       
       // echo "alert(\"THe item is: "."$itemName"."\");";
     ?>
