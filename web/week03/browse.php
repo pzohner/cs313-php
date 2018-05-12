ec9d45129d65adfd13037eb40e06933622c31e4a
Porter <slugsaver@gmail.com>
2018-05-12 08:26:12 -0700

    working on week03

--- web/week03/browse.php
+++ web/week03/browse.php
@@ -13,8 +13,8 @@
 	<!-- Including my personal styles -->
   <link href="styles.css" rel="stylesheet" type="text/css">
   
-<script>
   <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+<script>
 
 $(document).ready(function(){
     $('.addToCart').click(function(){
