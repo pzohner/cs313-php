4762a17b504e3d7056c1436d5980a4177b7714f1
Porter <slugsaver@gmail.com>
2018-05-21 14:26:32 -0700

    working on plsql database connection

--- web/week05/psqlTest.php
+++ web/week05/psqlTest.php
@@ -10,8 +10,15 @@
 <h1> psql test </h1>
 <?php
 try {
-    $user = 'cdsnhommsapxtd';
-    $password = 'b708eeb6d92062634b8a1e535954759f6ff4d1ddd06d83c7ed3a6acbbee31325';
+$dbUrl = getenv('DATABSE_URL');
+
+$dboptions = parse_url($dbUrl);
+
+$user = $dboptions['user'];
+$password = $dboptions['pass'];
+
+    // $user = 'cdsnhommsapxtd';
+    // $password = 'b708eeb6d92062634b8a1e535954759f6ff4d1ddd06d83c7ed3a6acbbee31325';
 
     $db = new PDO('pgsql:host=ec2-54-235-109-37.compute-1.amazonaws.com;port=5432;dbname=de9dr91rnaase1', $user, $password);
     echo 'PLSQL connection successful';
