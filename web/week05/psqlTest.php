65adc5fe066fdc2a53b8058329d287f53339214a
Porter <slugsaver@gmail.com>
2018-05-21 13:58:18 -0700

    working on plsql database connection

---
+++ web/week05/psqlTest.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title> Team03</title>
+   
+</head>
+
+<body>
+
+<?php
+try {
+    $user = 'root';
+    $password = '$otpWug2';
+
+    $db = new PDO('pgsql:host=127.0.0.1;dbname=myTestDB', $user, $password);
+    echo 'PLSQL connection successful';
+} catch (PDFEXCEPTION $ex)
+{
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+}
+
+?>
+
+
+
+
+
+  <br><br>
+  
+<input type="submit" value="Submit">
+</form>
+</html>
+</body>
