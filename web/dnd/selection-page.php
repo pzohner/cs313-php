bf03a2f264cd6f13c76b318fb299ff90228daa79
Porter <slugsaver@gmail.com>
2018-05-26 11:53:48 -0700

    working on week05 prove project 1 setup

--- web/dnd/selection-page.php
+++ web/dnd/selection-page.php
@@ -67,7 +67,7 @@ function isCharacterSelected() {
         
             # PLAYER SELECTION
             echo '<div id="character-selection">';
-            echo '<span> Choose a character...</span><br>';
+            echo '<span> Choose a character...</span>';
             foreach ($db->query('SELECT avatarname, imgpath FROM character') as $row)
             {
                 #print out all characters from the database
