115158adbcc1a89a6ebdc27f8bedc74542dd5a83
Porter <slugsaver@gmail.com>
2018-05-26 10:54:48 -0700

    working on week05 prove project 1 setup

--- web/dnd/character-creation.php
+++ web/dnd/character-creation.php
@@ -12,103 +12,17 @@
 
 	<!-- Including my personal styles -->
     <link href="styles.css" rel="stylesheet" type="text/css">
-    <script>
-
-
-    $(document).ready(function () {
-        // Disable create game button until they have selected a DM profile
-        $("#game-creation").attr("disabled", "disabled");
-
-    })
-
-function isDMSelected() {
-    var dmradio = document.getElementsByClassName("dmradio");
-
-    for(var i = 0; i < dmradio.length; i++) {
-        if (dmradio[i].checked) {
-            document.getElementById("game-creation").disabled = false;
-        }
-    }
-}
-
-function isCharacterSelected() {
-    var characterradio = document.getElementsByClassName("characterradio");
-
-    for(var i = 0; i < characterradio.length; i++) {
-        if (characterradio[i].checked) {
-            document.getElementById("game-creation").disabled = true;
-        }
-    }
-}
-</script>
+    
 </head>
 <body>
-    <div id='selection'> 
-        <span> Select the DM or character you wish to use to play or create a new one! </span> 
-        
-        <?php
-            try {
-            $dbUrl = getenv('DATABASE_URL');
+    <div id='character-creation'> 
+        <span> Choose a name and upload an image to create your character! </span> 
+        Character name: <input type="text">
+        Upload an image: <input type="file">
 
-            $dboptions = parse_url($dbUrl);
-
-            $user = $dboptions['user'];
-            $password = $dboptions['pass'];
-
-                $db = new PDO('pgsql:host=ec2-54-235-109-37.compute-1.amazonaws.com;port=5432;dbname=de9dr91rnaase1', $user, $password);
-            } catch (PDOEXCEPTION $ex)
-            {
-                echo 'Error!: ' . $ex->getMessage();
-                die();
-            }
-
-
-            echo '<div id="playerordm">';
-            # DM SELECTION
-            echo '<div id="dm-selection">';
-            echo '<span> Choose a Dungeon Masters Profile...<br>';
-            
-            foreach ($db->query('SELECT dmname, gamename, tableimgpath FROM dm') as $row)
-            {
-                #Print out all DM profiles from the database
-                echo '<input type="radio" name="player-selection" class="dmradio" onclick="isDMSelected()"> ' . $row['dmname'] . ' with game named ' . $row['gamename'] . ' with game board image at ' . $row['tableimgpath']. '<br/>';
-                
-            }
-            echo ' <button id="dm-creation" type="button"> Create new DM Profile </button><br>';
-            echo ' <button id="game-creation" type="button"> Create a new Game </button><br>';
-            echo '</div>';
-
-            echo '<hr>';
-
-            # PLAYER SELECTION
-            echo '<div id="character-selection">';
-            echo '<span> Choose a character...<br>';
-            foreach ($db->query('SELECT avatarname, imgpath FROM character') as $row)
-            {
-                #print out all characters from the database
-                echo '<input type="radio" name="player-selection" class="characterradio" onclick="isCharacterSelected()"> ' . $row['avatarname'] . ' with image at ' . $row['imgpath']. '<br/>';
-            }
-            echo '<button id="character-creation" type="button"> Create a new Character </button><br>';
-            echo '</div>';
-            echo '</div>';
-
-
-            # GAME SELECTION
-            echo '<div id="game-selection">';
-            echo '<span> Choose a game to join...<br>';
-            foreach ($db->query('SELECT games.gamename, dm.dmname FROM games, dm where games.dmid = dm.id ') as $row)
-            {
-                #print out all characters from the database
-                echo '<input type="radio" name="games-selection" class="gamesradio" onclick="isCharacterSelected()"> ' . $row['gamename'] . ' hosted by DM ' . $row['dmname']. '<br/>';
-            }
-            echo '<button id="enterGame" type="button"> Enter Game </button><br>';
-
-            echo '</div>';
-            
-        ?>
+    <button id="createCharacterComplete" type="button" onclick="window.location.href='selection-page.php'"> Finish </button>
         
     </div>
 
 </body>
-
 </html>
