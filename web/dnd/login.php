965afa95a95f26384ff2076376f446777f77f011
Porter <slugsaver@gmail.com>
2018-05-26 08:31:46 -0700

    working on plsql database connection

---
+++ web/dnd/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+
+	<!-- <script src="intro/scripts.js"></script> -->
+
+	<!-- Including bootstrap -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
+	 crossorigin="anonymous">
+
+	<!-- Including my personal styles -->
+	<!-- <link href="intro/styles.css" rel="stylesheet" type="text/css"> -->
+</head>
+<body>
+    <div id='login'> 
+        Username: <input type="text">
+        Password: <input type="text">
+
+    </div>
+</body>
+</html>
