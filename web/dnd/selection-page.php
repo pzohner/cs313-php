0741f67c9eb619e7896be1b959256f6679bfe67b
Porter <slugsaver@gmail.com>
2018-05-26 11:52:30 -0700

    working on week05 prove project 1 setup

--- web/dnd/selection-page.php
+++ web/dnd/selection-page.php
@@ -67,7 +67,7 @@ function isCharacterSelected() {
         
             # PLAYER SELECTION
             echo '<div id="character-selection">';
-            echo '<span> Choose a character...</span><br>';
+            echo '<span> Choose a character...<br>';
             foreach ($db->query('SELECT avatarname, imgpath FROM character') as $row)
             {
                 #print out all characters from the database
@@ -80,7 +80,7 @@ function isCharacterSelected() {
 
             # DM SELECTION
             echo '<div id="dm-selection">';
-            echo '<h5> Choose a Dungeon Masters Profile...</h5><br>';
+            echo '<span> Choose a Dungeon Masters Profile...<br>';
 
             foreach ($db->query('SELECT dmname, gamename, tableimgpath FROM dm') as $row)
             {
@@ -96,7 +96,7 @@ function isCharacterSelected() {
 
             # GAME SELECTION
             echo '<div id="game-selection">';
-            echo '<h5> Choose a game to join...</h5><br>';
+            echo '<span> Choose a game to join...<br>';
             foreach ($db->query('SELECT games.gamename, dm.dmname FROM games, dm where games.dmid = dm.id ') as $row)
             {
                 #print out all characters from the database
