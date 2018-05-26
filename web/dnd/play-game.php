7df0e71ce372f7d53663093b90a43950f4e4732b
Porter <slugsaver@gmail.com>
2018-05-26 13:13:59 -0700

    working on week05 prove project 1 setup

---
+++ web/dnd/play-game.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+
+	<!-- <script src="intro/scripts.js"></script> -->
+
+	<!-- Including bootstrap -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
+	 crossorigin="anonymous">
+
+	<!-- Including my personal styles -->
+    <link href="styles.css" rel="stylesheet" type="text/css">
+    
+</head>
+
+<?php
+try {
+    $dbUrl = getenv('DATABASE_URL');
+
+    $dboptions = parse_url($dbUrl);
+
+    $user = $dboptions['user'];
+    $password = $dboptions['pass'];
+
+        $db = new PDO('pgsql:host=ec2-54-235-109-37.compute-1.amazonaws.com;port=5432;dbname=de9dr91rnaase1', $user, $password);
+    } catch (PDOEXCEPTION $ex)
+    {
+        echo 'Error!: ' . $ex->getMessage();
+        die();
+    }
+
+
+
+    foreach ($db->query('SELECT games.gamename, dm.tableimgpath FROM games, dm where games.dmid = dm.id ') as $row)
+            {
+                #print out all characters from the database
+            echo '<body background="'. $row['tableimgpath'] . '">';
+            echo '<h1>' . $row['gamename'] . '<h1/>';
+            }
+
+   
+
+    echo '</body>';
+?>
+
+</html>
