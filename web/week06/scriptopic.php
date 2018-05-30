22c9a980f3fa6e7dd2121fcd7e57b8c438c9cf18
Porter <slugsaver@gmail.com>
2018-05-29 18:27:09 -0700

    working on week05 prove project 1 setup

---
+++ web/week06/scriptopic.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title> Team03</title>
+   
+</head>
+
+<body>
+<h1> psql test </h1>
+<form>
+Book:    <input type='text' name='scriptureselections' value=''> <br>
+Chapter: <input type='text' name='scriptureselections' value=''> <br> 
+Verse:   <input type='text' name='scriptureselections' value=''> <br> 
+<span> Content </span>
+<textarea rows="4" cols="50"></textarea> 
+</form>
+
+
+
+<?php
+try {
+$dbUrl = getenv('DATABASE_URL');
+
+$dboptions = parse_url($dbUrl);
+
+$user = $dboptions['user'];
+$password = $dboptions['pass'];
+
+    $db = new PDO('pgsql:host=ec2-54-235-109-37.compute-1.amazonaws.com;port=5432;dbname=de9dr91rnaase1', $user, $password);
+    echo 'PLSQL connection successful';
+} catch (PDOEXCEPTION $ex)
+{
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+}
+
+foreach ($db->query('SELECT username, password FROM note_user') as $row)
+{
+    echo 'user: ' . $row['username'] . 'password: ' . $row['password']. '<br/>';
+
+}
+?>
+
+
+
+
+
+  <br><br>
+  
+<input type="submit" value="Submit">
+</form>
+</html>
+</body>
