fd579b90827b2044ee6545976ed696186bcc03d1
Porter <slugsaver@gmail.com>
2018-05-29 18:51:41 -0700

    working on week05 prove project 1 setup

--- web/week06/scriptopic.php
+++ web/week06/scriptopic.php
@@ -8,45 +8,60 @@
 
 <body>
 <h1> psql test </h1>
-<form>
-Book:    <input type='text' name='scriptureselections' value=''> <br>
-Chapter: <input type='text' name='scriptureselections' value=''> <br> 
-Verse:   <input type='text' name='scriptureselections' value=''> <br> 
+
+<form action="scriptopic.php" method="Post">
+
+Book:    <input type='text' name='book' value=''> <br>
+Chapter: <input type='text' name='chapter' value=''> <br> 
+Verse:   <input type='text' name='verse' value=''> <br> 
 <span> Content </span>
 <textarea rows="4" cols="50"></textarea> 
-</form>
-
-
 
 <?php
-try {
-$dbUrl = getenv('DATABASE_URL');
+    if (isset($_POST['scriptureSubmit'])) {
+        try {
+            $dbUrl = getenv('DATABASE_URL');
+        
+            $dboptions = parse_url($dbUrl);
+        
+            $user = $dboptions['user'];
+            $password = $dboptions['pass'];
+        
+                $db = new PDO('pgsql:host=ec2-54-235-109-37.compute-1.amazonaws.com;port=5432;dbname=de9dr91rnaase1', $user, $password);
+            } catch (PDOEXCEPTION $ex)
+            {
+                echo 'Error!: ' . $ex->getMessage();
+                die();
+            }
+            
+            $db->query("INSERT INTO scriptures (book, chapter, verse, content) VALUES ('".$_POST["book"]."','".$_POST["chapter"]."','".$_POST["verse"]."','".$_POST["content"]."')");
+            // (\'$_POST['book']\', $_POST['chapter'], $_POST['verse'], \'$_POST['content']\')")
+    }
 
-$dboptions = parse_url($dbUrl);
+    try {
+    $dbUrl = getenv('DATABASE_URL');
 
-$user = $dboptions['user'];
-$password = $dboptions['pass'];
+    $dboptions = parse_url($dbUrl);
 
-    $db = new PDO('pgsql:host=ec2-54-235-109-37.compute-1.amazonaws.com;port=5432;dbname=de9dr91rnaase1', $user, $password);
-} catch (PDOEXCEPTION $ex)
-{
-    echo 'Error!: ' . $ex->getMessage();
-    die();
-}
+    $user = $dboptions['user'];
+    $password = $dboptions['pass'];
 
-foreach ($db->query('SELECT name FROM topic') as $row)
-{
-    echo "<div><input type='checkbox' name='scriptopic'>" . $row['name'] . "</div>";
-}
+        $db = new PDO('pgsql:host=ec2-54-235-109-37.compute-1.amazonaws.com;port=5432;dbname=de9dr91rnaase1', $user, $password);
+    } catch (PDOEXCEPTION $ex)
+    {
+        echo 'Error!: ' . $ex->getMessage();
+        die();
+    }
+
+    foreach ($db->query('SELECT name FROM topic') as $row)
+    {
+        echo "<div><input type='checkbox' name='scriptopic'>" . $row['name'] . "</div>";
+    }
+
 ?>
-
-
-
-
-
-  <br><br>
   
-<input type="submit" value="Submit">
+<input type="submit" value="submit" name="scriptureSubmit">
+
 </form>
 </html>
 </body>
