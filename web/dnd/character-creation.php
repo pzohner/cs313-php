bea4a97e5476f61ab81a75d012b73ebeae814ce6
Porter <slugsaver@gmail.com>
2018-06-02 15:35:30 -0700

    working on week05 prove project 1 setup

--- web/dnd/character-creation.php
+++ web/dnd/character-creation.php
@@ -47,6 +47,11 @@ if(isset($_POST["submit"])) {
         $uploadOk = 0;
     }
 }
+if (move_uploaded_file($_FILES["characterpic"]["tmp_name"], $target_file)) {
+    echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+} else {
+    echo "Sorry, there was an error uploading your file.";
+}
 
 
 ///////////////////////////
