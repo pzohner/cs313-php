19a4fa1698cf4eb5b3b92566a82f7f53b472a85f
Porter <slugsaver@gmail.com>
2018-06-09 10:46:48 -0700

    working on week07 multiuser integration and cleanup

--- web/dnd/play-game.php
+++ web/dnd/play-game.php
@@ -60,7 +60,7 @@ try {
             foreach ($db->query("SELECT avatarname, imgpath, userid, characterinuse FROM character where characterinuse = 'true'") as $row)
                     {
                         $nospaceName=preg_replace('/\s+/', '', $row['avatarname']);
-                        $nospaceName = $nospaceName . $users['username'];
+                        $nospaceName = $nospaceName . $_SESSION["currentUser"];
                         echo '<img id="character' . $nospaceName . '" src="' . $row['imgpath'] . '">';
                         // echo '<img id="character" src="' . $row['imgpath'] . '">';
 
