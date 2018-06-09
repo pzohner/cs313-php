477f32f4e379efef593da470eaa80c9e6f16191a
Porter <slugsaver@gmail.com>
2018-06-09 09:26:58 -0700

    working on week07 multiuser integration and cleanup

--- web/dnd/play-game.php
+++ web/dnd/play-game.php
@@ -9,7 +9,7 @@ session_start();
 	<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
 
 	<!-- <script src="intro/scripts.js"></script> -->
-    <meta http-equiv="Refresh" content="3">
+    <meta http-equiv="Refresh" content="30">
 	<!-- Including bootstrap -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
 	 crossorigin="anonymous">
