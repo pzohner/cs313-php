b25196bcdb7c250cdd70536123d267c06a4e3f61
Porter <slugsaver@gmail.com>
2018-06-09 17:10:50 -0700

    working on week07 multiuser integration and cleanup

--- web/dnd/play-game.php
+++ web/dnd/play-game.php
@@ -57,13 +57,14 @@ try {
     foreach ($db->query('SELECT id, username from users') as $users) {
         // if ($users['username'] == $_SESSION["currentUser"]) {
             $id = $users['id'];
-            foreach ($db->query("SELECT avatarname, imgpath, characterinuse FROM character where characterinuse = 'true' AND userid = '$id'") as $row)
+            foreach ($db->query("SELECT avatarname, imgpath, characterinuse FROM character where characterinuse = 'true' AND userid = '$id' AND avatarname = '$currentCharacter'") as $row)
                     {
                         $nospaceName=preg_replace('/\s+/', '', $row['avatarname']);
                         echo '<img id="character' . $nospaceName . $users['username'] . '" src="' . $row['imgpath'] . '">';
                         // echo '<img id="character" src="' . $row['imgpath'] . '">';
 
                     }
+                    
                 // }
             }
             $javascriptcharacter = $nospaceName . $_SESSION["currentUser"];
